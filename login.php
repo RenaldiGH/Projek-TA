37ea4acdf18d2cee3461dffb31ca62c9961a12c9
nesyarrahmaayu <nseeyyaa@gmail.com>
2026-08-18 14:15:52 +0700

    ubah

--- login.php
+++ login.php
@@ -24,4 +24,4 @@ require_once __DIR__ . '/includes/header.php';
 </div>
 </div>
 
-<?php require_once __DIR__ . '/includes/footer.php'; ?>
+
--- register.php
+++ register.php
@@ -6,11 +6,13 @@ require_once __DIR__ . '/includes/header.php';
 <div class="login-box">
     <h2>Register</h2>
     <form>
-        <input type="text" name="username" placeholder="Username" style="width:100%; margin-bottom:10px; padding:8px;">
+        <input type="text" name="Nama Lengkap" placeholder="Nama Lengkap" style="width:100%; margin-bottom:10px; padding:8px;">
         <input type="email" name="email" placeholder="Email" style="width:100%; margin-bottom:10px; padding:8px;">
         <input type="password" name="password" placeholder="Password" style="width:100%; margin-bottom:10px; padding:8px;">
+        <input type="password" name="konfirmasi password" placeholder="konfirmasi password" style="width:100%; margin-bottom:10px; padding:8px;">
+
         <button type="submit">Register</button>
     </form>
 </div>
 
-<?php require_once __DIR__ . '/includes/footer.php'; ?>
+
