fe4527777cf866de275ac1ec3d67e016afef7c66
nesyarrahmaayu <nseeyyaa@gmail.com>
2026-09-01 13:16:50 +0700

    ubah login

--- login.php
+++ login.php
@@ -4,36 +4,30 @@ require_once __DIR__ . '/includes/header.php';
 ?>
 <link rel="stylesheet" href="assets/css/style.css">
 <link rel="stylesheet" href="assets/css/login.css">
-<div class="login-container">
 
-  <div class="login-image"></div>
-  
-<div class="login-box">
-  <p class="login-sub"> selamat datang kembali </p>
-    <h1>Login</h1>
-    <form>
-        <div class="input-group">
+<!-- TAMBAHKAN PEMBUNGKUS BARU INI -->
+<div class="login-wrapper">
+    <div class="login-container">
+        <div class="login-image"></div>
+        
+        <div class="login-box">
+            <p class="login-sub">selamat datang kembali</p>
+            <h1>Login</h1>
+            <form>
+                <div class="input-group">
+                    <input type="text" name="username" placeholder="Username">
+                </div>
 
-        <input
-        type="text"
-        name="username"
-        placeholder="Username">
-         </div>
+                <!-- <br> dihapus karena jarak sudah diatur oleh margin-bottom .input-group -->
 
-         <br>
+                <div class="input-group">
+                    <input type="password" name="password" placeholder="Password">
+                </div>
 
-          <div class="input-group">
-           <input
-           type="password"
-           name="password"
-           placeholder="Password">
-         </div>
-
-         <div class="input-login">
-       <button type="submit">Login</button>
-      </div>
-    </form>
+                <div class="input-login">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-</div>
-
-
