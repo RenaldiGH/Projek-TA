07b89ece85352060f0768c4d07ec85caf6b9f967
ReyGH <emelburiq089@gmail.com>
2026-09-01 11:27:47 +0700

    ubah page login

--- login.php
+++ login.php
@@ -5,21 +5,31 @@ require_once __DIR__ . '/includes/header.php';
 <link rel="stylesheet" href="assets/css/style.css">
 <link rel="stylesheet" href="assets/css/login.css">
 <div class="login-container">
+
   <div class="login-image"></div>
+  
 <div class="login-box">
+  <p class="login-sub"> selamat datang kembali </p>
     <h1>Login</h1>
-    <br><br>
-    <hr>
-    <br> <br>
     <form>
         <div class="input-group">
-        <input type="text" name="username" placeholder="Username">
+
+        <input
+        type="text"
+        name="username"
+        placeholder="Username">
          </div>
+
          <br>
+
           <div class="input-group">
-           <input type="password" name="password" placeholder="Password">
+           <input
+           type="password"
+           name="password"
+           placeholder="Password">
          </div>
-         <div class="input-group">
+
+         <div class="input-login">
        <button type="submit">Login</button>
       </div>
     </form>
