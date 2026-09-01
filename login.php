0b6d14850613cfb78828400e7e82dc4ce4f8e431
nesyarrahmaayu <nseeyyaa@gmail.com>
2026-09-01 09:11:49 +0700

    ubah register

--- login.php
+++ login.php
@@ -2,8 +2,10 @@
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/includes/header.php';
 ?>
-
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/login.css">
 <div class="login-container">
+  <div class="login-image"></div>
 <div class="login-box">
     <h1>Login</h1>
     <br><br>
